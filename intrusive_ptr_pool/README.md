c887115cc4956f0b11882b498eca9480cdf7a071
Tao Jing <jing.tao@megatronix.co>
2025-08-07 11:02:20 +0800

    Initial add atomic_intrusive_ptr_pool
    Signed-off-by: Tao Jing <jing.tao@megatronix.co>

---
+++ intrusive_ptr_pool/README.md
@@ -0,0 +1,28 @@
+# Description
+Design a prototype of intra-process blackboard data-sharing mechanism. It supports multiple publisher/subscribers, while the subscribers only interest on the latest data, and "polls" the data at their own interval.
+
+## Feature
+- no any system heap allocation/free(refer to pre-request)
+- read/write is done simultaneously
+
+## pre-request
+- acutual data is allocated externally, e.g.: by 3rd memory pool; no system heap allocation/free can be assured if 3rd memory pool can ensure it when actual data value change
+- boost::intrusive_ptr requires embedding std::atomic<int> refcount to actual data struct
+- to support multiple read/write done simultaneously, the number of pre-allocated data should be NUM_OF_PUB + NUM_OF_SUB + 1
+
+# Implementation 1
+atomic_intrusive_ptr_pool.h:
+- construct by an array of pre-allocated data raw pointer
+- lock is implemented by spinlock of std::atomic_flag, as actual lock time is very low(only pointer update,etc)
+- std::atomic<int> refcount needs to be embedded in every data struct
+
+test_atomic_intrusive_ptr_pool.cpp:
+- (stress)test sample on multiple read/write
+
+# Implementation 2
+atomic_intrusive_ptr_pool_2.h:
+- this makes wrapper of actual data, which eliminates refcount needs to be embedded in every data struct
+- then this introduces can not deference the pointer, which needs to call the get() method to get the actual data pointer
+
+test_atomic_intrusive_ptr_pool_2.cpp:
+- (stress)test sample on multiple read/write
---
+++ intrusive_ptr_pool/atomic_intrusive_ptr_pool.h
@@ -0,0 +1,78 @@
+#ifndef __ATOMIC_INTRUSIVE_PTR_POOL_H__
+#define __ATOMIC_INTRUSIVE_PTR_POOL_H__
+
+#include <boost/intrusive_ptr.hpp>
+#include <vector>
+
+class SpinLock {
+private:
+    std::atomic_flag flag = ATOMIC_FLAG_INIT;
+public:
+    void lock() {
+        while (flag.test_and_set(std::memory_order_acquire)) {
+            // Spin wait
+        }
+    }
+    
+    void unlock() {
+        flag.clear(std::memory_order_release);
+    }
+};
+
+template<typename T>
+class AtomicIntrusivePtrPool {
+private:
+    boost::intrusive_ptr<T> m_ptr = nullptr;
+    SpinLock m_spinlock;
+    
+public:
+    AtomicIntrusivePtrPool(const std::vector<T*>& initial_ptrs) {
+        for (auto ptr : initial_ptrs) {
+            printf("Created new intrusive_ptr: %p\n", ptr);
+            m_map_ptrs[ptr] = boost::intrusive_ptr<T>(ptr);
+        }
+    }
+    
+    boost::intrusive_ptr<T> getOutput(void) {
+        auto it = std::find_if(m_map_ptrs.begin(), m_map_ptrs.end(), [](const auto& pair) {
+            return pair.first->refcount == 1;
+        });
+        //assert if it is not found
+        assert(it != m_map_ptrs.end());
+        auto new_ptr = m_map_ptrs[it->first];
+        return new_ptr;
+    }
+
+    void setOutput(const boost::intrusive_ptr<T> &new_ptr) {
+        m_spinlock.lock();
+        m_ptr = new_ptr;
+        m_spinlock.unlock();
+    }
+
+    boost::intrusive_ptr<T> getInput(void) {
+        m_spinlock.lock();
+        auto result = m_ptr;
+        m_spinlock.unlock();
+        return result;
+    }
+
+    void recycle(T* ptr) {
+        printf("Recycling pointer: %p\n", ptr);
+    }
+
+    std::map<T*, boost::intrusive_ptr<T>> m_map_ptrs;
+};
+
+// Required by boost::intrusive_ptr
+template<typename T>
+inline void intrusive_ptr_add_ref(T* p) {
+    ++p->refcount;
+    //std::cout << "Adding reference: " << p << ", new refcount: " << p->refcount << std::endl;
+}
+template<typename T>
+inline void intrusive_ptr_release(T* p) {
+    --p->refcount;
+    //std::cout << "Releasing reference: " << p << ", new refcount: " << p->refcount << std::endl;
+}
+
+#endif
